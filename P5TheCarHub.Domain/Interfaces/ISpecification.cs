d12970390bdf21e6b5bfe3e076c0a9eda745939f
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-08-09 19:55:44 -0400

    added specification interface

---
+++ P5TheCarHub.Domain/Interfaces/ISpecification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P5TheCarHub.Core.Interfaces
+{
+    public interface ISpecification<T>
+    {
+        bool IsSatisfiedBy(T candidate);
+    }
+}
