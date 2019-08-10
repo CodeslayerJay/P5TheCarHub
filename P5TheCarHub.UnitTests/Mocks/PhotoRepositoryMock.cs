dc3ec82170ed507c8c1ea3affa451d389ef15810
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-08-10 10:20:01 -0400

    added initial photorepository interface and tests

---
+++ P5TheCarHub.UnitTests/Mocks/PhotoRepositoryMock.cs
@@ -0,0 +1,56 @@
+﻿using P5TheCarHub.Core.Entities;
+using P5TheCarHub.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P5TheCarHub.UnitTests.Mocks
+{
+    public class PhotoRepositoryMock : IPhotoRepository
+    {
+        private List<Photo> _context;
+
+        public PhotoRepositoryMock()
+        {
+            _context = new List<Photo>();
+            SetupFakeData();
+        }
+
+        private void SetupFakeData()
+        {
+            var id = 0;
+            var seedData = new List<Photo>()
+            {
+                new Photo { Id = ++id, IsMain = true, VehicleId = 1 }
+            };
+
+            _context.AddRange(seedData);
+        }
+
+        private int SetId()
+        {
+            return _context.Count() + 1;
+        }
+
+        public Photo Add(Photo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Photo> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Photo GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ P5TheCarHub.UnitTests/ServicesTests/PhotoServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P5TheCarHub.UnitTests.ServicesTests
+{
+    public class PhotoServiceTests
+    {
+        private readonly PhotoService _photoService;
+
+        public PhotoServiceTests()
+        {
+            var photoRepo = new PhotoRepositoryMock();
+            _photoService = new PhotoService(photoRepo);
+        }
+    }
+}
