f8f73b8247b32f9fe24abf352522112e68fa185f
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-08-10 19:28:08 -0400

    added unit of work class

---
+++ P5TheCarHub.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5TheCarHub.Core.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        void SaveChanges();
+        Task SaveChangesAsync();
+
+    }
+}
