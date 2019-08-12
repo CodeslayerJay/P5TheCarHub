594a5e5153a0d72970b91cfc08636cb363047936
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-08-12 16:59:26 -0400

    updated logger to use generic type

--- P5TheCarHub.Domain/Interfaces/ILogging.cs
+++ P5TheCarHub.Domain/Interfaces/ILogging.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace P5TheCarHub.Core.Interfaces
 {
-    public interface ILogging
+    public interface ILogging<T>
     {
         void LogInformation(string message, params object[] args);
         void LogWarning(string message, params object[] args);
