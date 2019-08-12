79e5e02e53a08d96de276fbabccb4a8f236b79f1
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-08-12 16:57:34 -0400

    added logging interface

---
+++ P5TheCarHub.Domain/Interfaces/ILogging.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P5TheCarHub.Core.Interfaces
+{
+    public interface ILogging
+    {
+        void LogInformation(string message, params object[] args);
+        void LogWarning(string message, params object[] args);
+    }
+}
