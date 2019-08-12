7adff88af4dc93d39bc89b7805427fcf3e763816
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-08-11 21:19:35 -0400

    updated ui folder structure

--- P5TheCarHub.UI/Controllers/HomeController.cs
+++ P5TheCarHub.UI/Controllers/HomeController.cs
@@ -4,13 +4,16 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using P5TheCarHub.Core.Interfaces;
+
+using P5TheCarHub.Core.Interfaces.Services;
+
 using P5TheCarHub.UI.Models;
 
 namespace P5TheCarHub.UI.Controllers
 {
     public class HomeController : Controller
     {
+        
         public IActionResult Index()
         {
             return View();
