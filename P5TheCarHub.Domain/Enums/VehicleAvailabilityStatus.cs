ee1670b40e41b484b70ddecf0ef1e90761fda544
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-08-18 11:23:52 -0400

    added vehicle availability status per business rule to mark vehicles as available, sold, or not available

---
+++ P5TheCarHub.Domain/Enums/VehicleAvailabilityStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P5TheCarHub.Core.Enums
+{
+    public enum VehicleAvailabilityStatus
+    {
+        Available = 0,
+        NotAvailable,
+        Sold
+    }
+}
